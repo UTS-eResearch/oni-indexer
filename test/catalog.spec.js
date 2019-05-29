7d532d662953a412ca1f9fcab2f4f0b5dc07e32d
Moises Sacal <moisbo@gmail.com>
2019-05-29 11:34:21 +1000

    added 2 configurable indexable elements

--- test/catalog.spec.js
+++ test/catalog.spec.js
@@ -50,7 +50,7 @@ describe('catalog', function () {
     });
   });
 
-  describe('graph', function () {
+  describe('graph - dataset', function () {
     it('should load the graph into a dataset', function () {
 
       const caPath = path.join(process.cwd() + '/test-data', 'CATALOG.json');
@@ -67,14 +67,72 @@ describe('catalog', function () {
         return _.find(g['@type'], (gg) => gg === 'Dataset') ? g : undefined;
       });
 
-      const dataset = catalog.getGraphElement(fieldConfig['dataset'], graph, graphElement);
+      const dataset = catalog.getGraphElement(fieldConfig['Dataset'], graph, graphElement);
 
-      assert.strictEqual(dataset.record_type_s, 'dataset', 'Dataset not loaded');
+      assert.strictEqual(dataset.record_type_s, 'Dataset', 'Dataset not loaded');
     });
   });
 
-})
-;
+  describe('graph - person', function () {
+    it('should load the graph into a Person', function () {
+
+      const caPath = path.join(process.cwd() + '/test-data', 'CATALOG.json');
+      const ca = require(caPath);
+
+      const fieldConfig = catalog.config;
+
+      //TODO: Peter's idea is to convert everything into an array then it is safer to work to convert
+      const graph = _.each(ca['@graph'], (g) => {
+        return catalog.ensureObjArray(g);
+      });
+
+      let graphElement = _.find(graph, (g) => {
+        return _.find(g['@type'], (gg) => gg === 'Person') ? g : undefined;
+      });
+
+      const person = catalog.getGraphElement(fieldConfig['Person'], graph, graphElement);
+
+      assert.strictEqual(person.record_type_s, 'Person', 'Person not loaded');
+    });
+  });
+
+  describe('graph - catalog solr', function () {
+    it('should load the graph into a catalog solr array', function () {
+
+      const caPath = path.join(process.cwd() + '/test-data', 'CATALOG.json');
+      const ca = require(caPath);
+
+      const fieldConfig = catalog.config;
+
+      //TODO: Peter's idea is to convert everything into an array then it is safer to work to convert
+      const graph = _.each(ca['@graph'], (g) => {
+        return catalog.ensureObjArray(g);
+      });
+
+      const catalogSolr = {};
+      _.each(fieldConfig, (field, name) => {
+        let graphElement = _.filter(graph, (g) => {
+          return _.find(g['@type'], (gg) => gg === name) ? g : undefined;
+        });
+        if (graphElement) {
+          _.each(graphElement, (ge) => {
+            if (Array.isArray(catalogSolr[name])) {
+              catalogSolr[name].push(catalog.getGraphElement(fieldConfig[name], graph, ge));
+            } else {
+              catalogSolr[name] = [catalog.getGraphElement(fieldConfig[name], graph, ge)];
+            }
+          });
+        }
+      });
+
+      assert.strictEqual(catalogSolr.Dataset[0].record_type_s, 'Dataset', 'dataset not loaded');
+      assert.strictEqual(catalogSolr.Person[0].record_type_s, 'Person', 'person 1 not loaded');
+      assert.strictEqual(catalogSolr.Person[3].record_type_s, 'Person', 'person 1 not loaded');
+
+    });
+  });
+
+});
 
 after(() => {
   fs.remove(datacrateDirPath);
