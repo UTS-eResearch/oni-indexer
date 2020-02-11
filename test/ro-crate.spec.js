5805fc6f9dac389baa1e83c5795abea9b5239dab
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-02-11 13:45:30 +1100

    Checking in mods to the test fields.json for FOR indexing

--- test/ro-crate.spec.js
+++ test/ro-crate.spec.js
@@ -50,11 +50,9 @@ describe('create solr object', function () {
     expect(solrObject['Dataset'][0]['record_type_s'][0]).to.equal('Dataset');
     const dsSolr = solrObject['Dataset'][0];
 
-    expect(dsSolr).to.have.property("Dataset_about_facet");
+    expect(dsSolr).to.have.property("Dataset_about_facetmulti");
     expect(dsSolr).to.have.property("about");
-    console.log("about: " + dsSolr['about']);
-    const for_index = JSON.parse(dsSolr['about']);
-    console.log(for_index);
+    const for_index = JSON.parse('[' + dsSolr['about'] + ']');
     expect(for_index).to.be.an('array');
     expect(for_index).to.have.lengthOf(orig_fors.length);
 
