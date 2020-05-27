0e75e1b7a41ffb5d64e81120e307ffeb87d812d8
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-05-27 11:53:11 +1000

    Renamed commit-to-solr.js to oni-indexer.js, started merging config, added winston logging

--- oni-indexer.js
+++ oni-indexer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+
 const axios = require('axios');
 const _ = require('lodash');
 const CatalogSolr = require('./lib/CatalogSolr');
@@ -10,14 +11,31 @@ const OCFLRepository = require('ocfl').Repository;
 const uuidv1 = require('uuid/v1');
 const hasha = require('hasha');
 const prompts = require('prompts');
+const winston = require('winston');
 
-const DEFAULT_CONFIG = './config.json';
+const logger = winston.createLogger({
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
+
+logger.info("Logging has started");
+
+const DEFAULTS = {
+  'config': './config.json',
+  'retries': 10,
+  'retryInterval': 10,
+  'catalogFilename': 'ro-crate-metadata.jsonld',
+  'uriIds': 'hashpaths',
+  'hashAlgorithm': 'md5'
+};
 
 var argv = require('yargs')
     .usage('Usage: $0 [options]')
     .describe('c', 'Config file')
     .alias('c', 'config')
-    .default('c', DEFAULT_CONFIG)
+    .default('c', DEFAULTS['config'])
     .alias('i', 'item')
     .describe('i', 'Index a single item')
     .string('i')
@@ -25,80 +43,98 @@ var argv = require('yargs')
     .alias('h', 'help')
     .argv;
 
-
-// FIXME - do this with an async file read and don't make a bunch
-// of global variables
-
 const configPath = argv.config;
 
-
-if (!fs.existsSync(configPath)) {
-  console.error(`Please provide a valid config file path: ${configPath}`);
-  process.exit(1);
-}
-
-
-
-const configJson = fs.readJsonSync(configPath);
-const retry = configJson['retry'] || 10;
-const retryInterval = configJson['retryInterval'] || 10;
-const solrBase = configJson['solrBase'] || '';
-const solrUpdate = solrBase + '/update/json';
-const solrSchema = solrBase + '/schema';
-const solrPing = solrBase + '/admin/ping';
-const fieldConfig = fs.readJsonSync(configJson['fields']);
-const logLevel = configJson['logLevel'] || 4;
-const waitPeriod = configJson['waitPeriod'] || 0;
-const batchNum = configJson['batch'] || 1000;
-const catalogFilename = configJson['catalogFilename'] || 'CATALOG.json';
-const uriIds = configJson['uriIds'] || 'hashpaths';
-const hashAlgorithm = configJson['hashAlgorithm'] || 'md5';      
-const sourcePath = configJson['ocfl'];
-const dumpDir = configJson['dump'];
-
-const dryRun = configJson['dryRun'] || false;
-
 const sleep = ms => new Promise((r, j) => {
-  console.log('Waiting for ' + ms + ' seconds');
   setTimeout(r, ms * 1000);
 });
 
 
-main();
+main(argv);
 
 
 
-async function main () {
-
-
-
-  let indexer = new CatalogSolr();
-  if( !indexer.setConfig(fieldConfig) ) {
+async function main (argv) {
+  let cf;
+  
+  try {
+    cf = await fs.readJson(argv.config);
+  } catch(e) {
+    logger.error("Configuration error");
+    logger.error(e);
     return;
   }
 
-  const solrUp = await checkSolr();
+  for ( let key in DEFAULTS ) {
+    if( !cf[key] ) {
+      cf[key] = DEFAULTS[key];
+      logger.info(`Using default config ${key}: ${cf[key]}`);
+    }
+  }
+
+  if( cf['log'] ) {
+    logger.add(new winston.transports.File(cf['log']));
+    logger.info(`Added log file: ${JSON.stringify(cf['log'])}`);
+  }
+
+  const indexer = new CatalogSolr();
+
+  if( !indexer.setConfig(cf['fields']) ) {
+    return;
+  }
+
+  const solrUpdate = cf['solrBase'] + '/update/json';
+
+  const solrUp = await checkSolr(cf['solrBase'] + '/admin/ping', cf['retries'], cf['retryInterval']);
 
   if( solrUp ) {
-    console.log("Solr is responding to pings");
-    if( configJson['purge'] ) {
-      await purgeSolr();
+    if( cf['purge'] ) {
+      await purgeSolr(solrUpdate);
     }
 
-
-    if( configJson['updateSchema'] ) {
-      await updateSchema(solrSchema, configJson['schema']);
+    if( cf['updateSchema'] ) {
+      //TODO
+      //await updateSchema(cf['solrBase'] + '/schema', indexer.schema());
     }
 
 
     const records = await loadFromOcfl(sourcePath);
     await indexRecords(records);
+
+    // TODO; write out the facets config 
+    // from indexer.facets() (or keep that logic in this script rather than the library)
+
   } else {
-    console.log("Couldn't connect to Solr");
+    logger.error("Couldn't connect to Solr");
   }
 }
 
 
+
+
+
+async function checkSolr(solrPing) {
+  for( let i = 0; i < retry; i++ ) {
+    logger.info(`Pinging Solr - attempt ${0} of ${retry}`)  
+    try {
+      const response = await axios({
+        url: solrPing,
+        method: 'get',
+        responseType: 'json'
+      });
+      if( response.status == 200 ) {
+        if( response.data['status'] === 'OK' ) {
+          return true;
+        }
+      }
+    } catch(e) {
+      logger.info(`Solr ping failed`);
+    }
+    await sleep(retryInterval);
+  }
+  logger.info(`Maximum connection attempts ${retry}`);
+  return false;
+}
 
 
 
@@ -120,7 +156,7 @@ function commitDocs(solrURL, args) {
 }
 
 function updateDocs(solrURL, coreObjects) {
-  console.log('updateDocs: ' + solrURL);
+  logger.info('updateDocs: ' + solrURL);
   return axios({
     url: solrURL + '/docs',
     method: 'post',
@@ -141,7 +177,7 @@ async function updateSchema(solrURL, schemaFile) {
 
   for( const type of Object.keys(schemaConf) ) {
     for( const field of schemaConf[type] ) {
-      //console.log(`Setting schema field ${type} ${JSON.stringify(field)}`);
+      logger.info(`Setting schema field ${type} ${JSON.stringify(field)}`);
       await setSchemaField(solrURL, type, field);
     }
   }
@@ -157,22 +193,22 @@ async function setSchemaField(solrURL, fieldtype, schemaJson) {
   // can't be replaced, so I'm trying to delete them and ignoring errors.
 
   if( fieldtype === 'copyfield' ) {
-    console.log(`Deleting copyfield ${JSON.stringify(schemaJson)}`);
+    logger.info(`Deleting copyfield ${JSON.stringify(schemaJson)}`);
     await tryDeleteCopyField(solrURL, schemaJson);
     schemaAPIJson['add-copy-field'] = schemaJson;
   } else {
     const apifield = ( fieldtype === 'field' ) ? 'field' : 'dynamic-field';
     if( await schemaFieldExists(url, name) ) {
-      console.log(`Schema: replacing ${fieldtype} ${name}`);
+      logger.info(`Schema: replacing ${fieldtype} ${name}`);
       schemaAPIJson['replace-' + apifield] = schemaJson;
     } else {    
-      console.log(`Schema: adding ${fieldtype} ${name}`);
+      logger.info(`Schema: adding ${fieldtype} ${name}`);
       schemaAPIJson['add-' + apifield] = schemaJson;
     }
   }
 
   try {
-    console.log(`Posting to schema API ${url} ${JSON.stringify(schemaAPIJson)}`);
+    logger.info(`Posting to schema API ${url} ${JSON.stringify(schemaAPIJson)}`);
     const response = await axios({
       url: solrURL,
       method: 'post',
@@ -182,15 +218,14 @@ async function setSchemaField(solrURL, fieldtype, schemaJson) {
       'Content-Type': 'application/json; charset=utf-8'
       }
     });
-    //console.log("Response: " + response.status);
   } catch(e) {
-    console.log("Error updating schema");
-    console.log(`URL: ${url}`);
-    console.log(`schemaAPIJson: ${JSON.stringify(schemaAPIJson)}`);
+    logger.error("Error updating schema");
+    logger.error(`URL: ${url}`);
+    logger.silly(`schemaAPIJson: ${JSON.stringify(schemaAPIJson)}`);
     if( e.response ) {
-      console.log(`${e.response.status} ${e.response.statusText}`);
+      logger.error(`${e.response.status} ${e.response.statusText}`);
     } else {
-      console.log(e);
+      logger.error(e);
     }
   }
 }
@@ -203,14 +238,14 @@ async function schemaFieldExists(solrURL, field) {
       method: 'get',
       responseType: 'json', 
     });
-    console.log("Schema field " + field + " already exists");
+    logger.info("Schema field " + field + " already exists");
     return true;
   } catch(e) {
     if( e.response.status === 404 ) {
-      console.log("Schema field " + field + " not found");
+      logger.error("Schema field " + field + " not found");
       return false;
     } else {
-      console.log("unknown error " + e);
+      logger.error("unknown error " + e);
       throw(e);
       return false;
     } 
@@ -228,19 +263,19 @@ async function tryDeleteCopyField(solrURL, copyFieldJson) {
       'Content-Type': 'application/json; charset=utf-8'
       }
     });
-    console.log("copyfield removed");
+    logger.info("copyfield removed");
     return true;
   } catch(e) {
     if( e.response ) {
       if( e.response.status === 404 ) {
-        console.log("Schema field " + field + " not found");
+        logger.error("Schema field " + field + " not found");
         return false;
       } else {
-        console.log("copy field delete error " + e.response.status);
+        logger.error("copy field delete error " + e.response.status);
         return false;
       }
     } else { 
-      console.log("unknown error " + e);
+      logger.error("unknown error " + e);
       throw(e);
       return false;
     } 
@@ -248,35 +283,9 @@ async function tryDeleteCopyField(solrURL, copyFieldJson) {
 }
 
 
-async function checkSolr() {
-  for( let i = 0; i < retry; i++ ) {
-    console.log(`Pinging Solr - attempt ${0} of ${retry}`)  
-    try {
-      const response = await axios({
-        url: solrPing,
-        method: 'get',
-        responseType: 'json'
-      });
-      if( response.status == 200 ) {
-        if( response.data['status'] === 'OK' ) {
-          return true;
-        }
-      }
-    } catch(e) {
-      console.log(`Solr ping failed`);
-    }
-    await sleep(retryInterval);
-  }
-  console.log(`Maximum connection attempts ${retry}`);
-  return false;
-}
 
 
-
-
-
-
-async function purgeSolr() {
+async function purgeSolr(solrUpdate) {
 
   try {
     const response = await axios({
@@ -288,17 +297,17 @@ async function purgeSolr() {
         'Content-Type': 'application/json; charset=utf-8'
       }
     });
-    console.log("All solr documents deleted.");
+    logger.info("All solr documents deleted.");
     return true
   } catch(e) {
     if( e.response ) {
-      console.log("Solr error");
+      logger.error("Solr error");
 
-      console.log(e.response.status);
+      logger.error(e.response.status);
       return false;
     } else {
-      console.log("General error");
-      console.log(e);
+      log.error("General error");
+      log.error(e);
       process.exit(-1);
     }
   }
@@ -329,7 +338,7 @@ async function loadFromOcfl(repoPath) {
           ocflObject: object
         });
       } else {
-       // console.log(`No ${catalogFilename} found in ${object['path']}`);
+       logger.error(`No ${catalogFilename} found in ${object['path']}`);
       }
     }
   }
@@ -351,7 +360,7 @@ async function dumpDocs(jsonld, solrDocs) {
 async function solrObjects(records) {
   let indexer = new CatalogSolr();
   if( !indexer.setConfig(fieldConfig) ) {
-    console.log("Solr config error");
+    logger.error("Solr config error");
     return [];
   }
   const solrDocs = [];
@@ -363,7 +372,6 @@ async function solrObjects(records) {
         return path.join(sourcePath, record['path'], relpath);
       });
       if (docs) {
-        console.log(`Got solr docs of type: ${Object.keys(docs)}`);
         if( dumpDir ) {
           await dumpDocs(record, docs);
         }
@@ -377,7 +385,7 @@ async function solrObjects(records) {
                 if( dataset['id'] && Array.isArray(dataset['id']) ) {
                   dataset['uri_id'] = dataset['id'][0];
                 } else {
-                  console.log("Warning: couldn't find id for uri_id");
+                  logger.error("Couldn't find id for uri_id");
                 }
               };
               solrDocs.push(dataset);
@@ -391,10 +399,10 @@ async function solrObjects(records) {
       
       }
     } catch(e) {
-      console.log("Error converting ro-crate to solr");
-      console.log(`path: ${record['path']}`);
-      console.log(`uri_id: ${record['uri_id']}`);
-      console.log(e);
+      logger.error("Error converting ro-crate to solr");
+      logger.error(`path: ${record['path']}`);
+      logger.error(`uri_id: ${record['uri_id']}`);
+      logger.error(e);
     }
 
   }
@@ -403,48 +411,6 @@ async function solrObjects(records) {
 }
 
 
-
-// Note: I think this could use a rewrite to put it into a more easy-to-understand
-// async/await style rather than relying on explicit Promise operations
-
-
-async function commitBatches (records) {
-  console.log("updating " + records.length + " records");
-  const batch = _.chunk(records, batchNum);
-
-  batch.reduce((promise, records, index) => {
-    return promise.then(async () => {
-      if (logLevel >= 4) {
-        reportMemUsage();
-      }
-      const solrDocs = await solrObjects(records);
-      if( dryRun ) {
-        console.log("Dry-run mode, not committing");
-        return Promise.resolve();
-      }
-      return updateDocs(solrUpdate, solrDocs).then(async () => {
-        if (waitPeriod) {
-          const waited = await sleep(waitPeriod);
-        }
-        console.log(`batch ${index + 1} of ${batch.length} : Update docs`);
-        if (index >= batch.length - 1) {
-          return commitDocs(solrUpdate, '?commit=true&overwrite=true').then(() => {
-            return Promise.resolve();
-          });
-        }
-        return Promise.resolve();
-      });
-    }).catch((e) => {
-      console.log("Update failed: " + e);
-      if( e.response ) {
-        console.log(e.response.status);
-      } else {
-        console.log(JSON.stringify(e));
-      }
-    })
-  }, Promise.resolve());
-
-}
 
 // this is a basic version which loops through the record, indexes and commits
 // them one at a time, so a single bad record won't prevent an entire indexing
@@ -459,25 +425,17 @@ async function indexRecords(records) {
     try {
       await updateDocs(solrUpdate, [ doc ]);
       await commitDocs(solrUpdate, '?commit=true&overwrite=true');
-      console.log(`Indexed ${doc['record_type_s']} ${doc['id']}`);
+      logger.info(`Indexed ${doc['record_type_s']} ${doc['id']}`);
     } catch(e) {
-      console.log("Update failed: " + e);
+      logger.error("Update failed: " + e);
       if( e.response ) {
-        console.log(e.response.status);
+        logger.error(e.response.status);
       }      
     }
   }
 }
 
 
-function reportMemUsage() {
-   console.log(`Using: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100} MBs`);
-}
 
-async function dumpSolrSync(solr) {
-  const uuname = path.join('test-data', uuidv1() + '.json');
-  fs.writeJsonSync(uuname, solr, { spaces: 2 });
-  console.log(`Wrote solr docs to ${uuname}`); 
-}
 
 
