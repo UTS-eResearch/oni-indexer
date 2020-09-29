ad94b480d75e2a083477b18d63b9a82b8e0fd44e
Moises Sacal <moisbo@gmail.com>
2020-09-29 14:26:06 +1000

    added options for not purging the index, see package.json scripts

--- docker-run.sh
+++ docker-run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 PORT=8090
-VOLUMES_FROM=en-oni-express
 DCK_PRE=en
+VOL_OCFL="/Users/moises/source/github/uts-eresearch/heurist2ro-crate/en_ocfl/"
+VOL_CONFIG="/Users/moises/source/github/uts-eresearch/oni-express/config/"
 NETWORK=main
 
-docker run --rm -p 127.0.0.1:${PORT}:${PORT} \
---volumes-from ${VOLUMES_FROM} \
---name ${DCK_PRE}-oni-indexer \
---network ${NETWORK} \
--d \
-oni-indexer
+DOCKER_CMD="docker run --rm -p 127.0.0.1:${PORT}:${PORT} -v ${VOL_CONFIG}:/etc/share/config -v ${VOL_OCFL}:/etc/share/ocfl --name ${DCK_PRE}-oni-indexer --network ${NETWORK} -d --entrypoint /usr/src/app/oni-indexer.js oni-indexer"
+
+if [ "$1" == "clean" ];then
+  DOCKER_CMD+=" -c /etc/share/config/indexer.json -p true"
+else
+  DOCKER_CMD+=" -c /etc/share/config/indexer.json -p false"
+fi
+
+${DOCKER_CMD}
