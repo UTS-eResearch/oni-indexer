0c5bbafbdf944b6fe0ea9df7f5dba84d92e4b99f
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-10-07 11:52:05 +1100

    Added a separate test script for resolving items which checks some values from the criminal characters dataset

---
+++ test/resolve-items.spec.js
@@ -0,0 +1,61 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const _ = require('lodash');
+const path = require('path');
+const fs = require('fs-extra');
+const CatalogSolr = require('../lib/CatalogSolr');
+const rocrate = require('ro-crate');
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
+
+async function initIndexer(configFile) {
+  const cf = await fs.readJson(configFile);
+  const indexer = new CatalogSolr(logger);
+  indexer.setConfig(cf['fields']);
+  return indexer;
+}
+
+
+
+describe('indexing values with item resolution', function () {
+  const test_data = path.join(process.cwd(), 'test-data', 'criminals');
+
+  it('can index the criminal characters dataset', async function () {
+    const cf = path.join(test_data, 'indexer.json');
+    const ca = await fs.readJson(path.join(test_data, 'ro-crate-metadata.json'));
+    const indexer = await initIndexer(cf);
+
+    const solrDocs = await indexer.createSolrDocument(ca, '@graph');
+    expect(solrDocs['Person']).to.not.be.empty;
+    const persons = solrDocs['Person'];
+
+
+
+    const birthPlacesJson = await fs.readFile(path.join(test_data, "birthPlaces.json"));
+    const birthPlaces = JSON.parse(birthPlacesJson);
+
+    // note: the members of the solr doc objects returned by createSolrDocument
+    // are weird JS objects which look like single-element arrays when 
+    // stringified, which is what the [0] in the filter is for. This is bad
+    // and needs fixing.
+
+    for( let id in birthPlaces ) {
+      const solrDoc = persons.filter((d) => { return d['id'][0] === id });
+      expect(solrDoc).to.not.be.empty;
+      const bp = solrDoc[0]['birthPlace'];
+      expect(bp).to.equal(birthPlaces[id]);
+    }
+
+  });
+
+
+// #person__VICFP_18551934_1_573
+
+});
