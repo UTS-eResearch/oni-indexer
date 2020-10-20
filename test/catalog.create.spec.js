48f48934e103a097cc2b6fb7e101e3cf5961b6c7
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-10-20 11:45:13 +1100

    Renaming CatalogSolr to ROCrateIndexer

--- test/catalog.create.spec.js
+++ test/catalog.create.spec.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 const path = require('path');
 const fs = require('fs-extra');
 const randomize = require('../lib/randomize');
-const CatalogSolr = require('../lib/CatalogSolr');
+const ROCrateIndexer = require('../lib/ROCrateIndexer');
 const winston = require('winston');
 
 const logger = winston.createLogger({
@@ -26,15 +26,15 @@ before(async () => {
   sourcedata = await randomize.loadsourcedata('./vocabularies');
 });
 
-let catalogjson = null;
+let jsonld = null;
 
 describe('create random ro-crates', function () {
   it('randomize 1 rocrate', async function () {
     datapubs = randomize.randdatapubs(1, sourcedata);
     const id = await randomize.makedir(rocrateDirPath);
     await randomize.makerocrate(rocrateDirPath, datapubs[0], id);
-    catalogjson = await fs.readJson(path.join(rocrateDirPath, id, 'ro-crate-metadata.jsonld'));
-    assert.notStrictEqual(catalogjson['@graph'], undefined, 'ro-crate not created');
+    jsonld = await fs.readJson(path.join(rocrateDirPath, id, 'ro-crate-metadata.jsonld'));
+    assert.notStrictEqual(jsonld['@graph'], undefined, 'ro-crate not created');
   });
 });
 
@@ -44,13 +44,13 @@ describe.skip('catalog', function () {
   let catalog = {};
 
   before(function () {
-    catalog = new CatalogSolr(logger);
+    indexer = new ROCrateIndexer(logger);
   });
 
   describe('load config fields', function () {
     it('should get config fields', function () {
       const fields = require(fieldsPath);
-      const isConfig = catalog.setConfig(fields);
+      const isConfig = indexer.setConfig(fields);
       assert.strictEqual(isConfig, true, 'Config not complete');
     });
   });
@@ -61,13 +61,13 @@ describe.skip('catalog', function () {
       const caPath = path.join(process.cwd() + '/test-data', 'CATALOG.json');
       const ca = require(caPath);
 
-      const fieldConfig = catalog.config;
+      const fieldConfig = indexer.config;
 
       let graphElement = _.find(graph, (g) => {
         return _.find(g['@type'], (gg) => gg === 'Dataset') ? g : undefined;
       });
 
-      const dataset = catalog.getGraphElement(fieldConfig['Dataset'], graph, graphElement);
+      const dataset = indexer.getGraphElement(fieldConfig['Dataset'], graph, graphElement);
 
       assert.strictEqual(dataset.record_type_s, 'Dataset', 'Dataset not loaded');
     });
@@ -79,86 +79,86 @@ describe.skip('catalog', function () {
       const caPath = path.join(process.cwd() + '/test-data', 'CATALOG.json');
       const ca = require(caPath);
 
-      const fieldConfig = catalog.config;
+      const fieldConfig = indexer.config;
 
       let graphElement = _.find(graph, (g) => {
         return _.find(g['@type'], (gg) => gg === 'Person') ? g : undefined;
       });
 
-      const person = catalog.getGraphElement(fieldConfig['Person'], graph, graphElement);
+      const person = indexer.getGraphElement(fieldConfig['Person'], graph, graphElement);
 
       assert.strictEqual(person.record_type_s, 'Person', 'Person not loaded');
     });
   });
 
-  describe('graph - catalog solr', function () {
-    it('should load the graph into a catalog solr array', function () {
+  describe('graph - indexer solr', function () {
+    it('should load the graph into a indexer solr array', function () {
 
       const caPath = path.join(process.cwd() + '/test-data', 'CATALOG.json');
       const ca = require(caPath);
 
-      const fieldConfig = catalog.config;
+      const fieldConfig = indexer.config;
 
       //Peter's idea is to convert everything into an array then it is safer to work to convert
       const graph = _.each(ca['@graph'], (g) => {
-        return catalog.ensureObjArray(g);
+        return indexer.ensureObjArray(g);
       });
 
-      const catalogSolr = {};
+      const solrJSON = {};
       _.each(fieldConfig, (field, name) => {
         let graphElement = _.filter(graph, (g) => {
           return _.find(g['@type'], (gg) => gg === name) ? g : undefined;
         });
         if (graphElement) {
           _.each(graphElement, (ge) => {
-            if (Array.isArray(catalogSolr[name])) {
-              catalogSolr[name].push(catalog.getGraphElement(fieldConfig[name], graph, ge));
+            if (Array.isArray(solrJSON[name])) {
+              solrJSON[name].push(indexer.getGraphElement(fieldConfig[name], graph, ge));
             } else {
-              catalogSolr[name] = [catalog.getGraphElement(fieldConfig[name], graph, ge)];
+              solrJSON[name] = [indexer.getGraphElement(fieldConfig[name], graph, ge)];
             }
           });
         }
       });
 
-      assert.strictEqual(catalogSolr.Dataset[0].record_type_s, 'Dataset', 'dataset not loaded');
-      assert.strictEqual(catalogSolr.Person[0].record_type_s, 'Person', 'person 1 not loaded');
-      assert.strictEqual(catalogSolr.Person[3].record_type_s, 'Person', 'person 1 not loaded');
+      assert.strictEqual(solrJSON.Dataset[0].record_type_s, 'Dataset', 'dataset not loaded');
+      assert.strictEqual(solrJSON.Person[0].record_type_s, 'Person', 'person 1 not loaded');
+      assert.strictEqual(solrJSON.Person[3].record_type_s, 'Person', 'person 1 not loaded');
 
     });
   });
 
-  describe('farm to freeways graph - catalog solr', function () {
-    it('should load the graph into a catalog solr array', function () {
+  describe('farm to freeways graph - indexer solr', function () {
+    it('should load the graph into a indexer solr array', function () {
 
       const caPath = path.join(process.cwd() + '/test-data', 'FARMTOFREEWAYS_CATALOG.json');
       const ca = require(caPath);
 
-      const fieldConfig = catalog.config;
+      const fieldConfig = indexer.config;
 
       //Peter's idea is to convert everything into an array then it is safer to work to convert
       const graph = _.each(ca['@graph'], (g) => {
-        return catalog.ensureObjArray(g);
+        return indexer.ensureObjArray(g);
       });
 
-      const catalogSolr = {};
+      const solrJSON = {};
       _.each(fieldConfig, (field, name) => {
         let graphElement = _.filter(graph, (g) => {
           return _.find(g['@type'], (gg) => gg === name) ? g : undefined;
         });
         if (graphElement) {
           _.each(graphElement, (ge) => {
-            if (Array.isArray(catalogSolr[name])) {
-              catalogSolr[name].push(catalog.getGraphElement(fieldConfig[name], graph, ge));
+            if (Array.isArray(solrJSON[name])) {
+              solrJSON[name].push(indexer.getGraphElement(fieldConfig[name], graph, ge));
             } else {
-              catalogSolr[name] = [catalog.getGraphElement(fieldConfig[name], graph, ge)];
+              solrJSON[name] = [indexer.getGraphElement(fieldConfig[name], graph, ge)];
             }
           });
         }
       });
 
-      assert.strictEqual(catalogSolr.Dataset[0].record_type_s, 'Dataset', 'dataset not loaded');
-      assert.strictEqual(catalogSolr.Person[0].record_type_s, 'Person', 'person 1 not loaded');
-      assert.strictEqual(catalogSolr.Person[3].record_type_s, 'Person', 'person 1 not loaded');
+      assert.strictEqual(solrJSON.Dataset[0].record_type_s, 'Dataset', 'dataset not loaded');
+      assert.strictEqual(solrJSON.Person[0].record_type_s, 'Person', 'person 1 not loaded');
+      assert.strictEqual(solrJSON.Person[3].record_type_s, 'Person', 'person 1 not loaded');
 
     });
   });
